73fe89a7dc4ed3de6141f61ab97a320bcfb97d24
edwloef <edwin.frank.loeffler@gmail.com>
2026-05-02 21:53:50 +0200

    simplify `Stations`

--- src/io/Stations.cpp
+++ src/io/Stations.cpp
@@ -30,7 +30,7 @@ tsunami_lab::io::Stations::Stations(std::ifstream i_file) {
 
 void tsunami_lab::io::Stations::output(t_real i_dxy, t_real i_simTime,
                                        patches::WavePropagation *i_waveProp) {
-    if (i_simTime - m_lastOutput < m_outputFreq) {
+    if (i_simTime >= nextOutputTime()) {
         return;
     }
 
