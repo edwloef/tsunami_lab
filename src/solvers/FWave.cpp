20cfc9e87d0761c5eb770212c3175c0a7dedd7b4
edwloef <edwin.frank.loeffler@gmail.com>
2026-05-25 08:12:33 +0200

    more micro-optimizations

--- src/solvers/FWave.cpp
+++ src/solvers/FWave.cpp
@@ -53,17 +53,18 @@ void tsunami_lab::solvers::FWave::netUpdates(t_real i_hL, t_real i_hR,
     t_real z2[2] = {alpha2, alpha2 * delta2};
 
     // distribute waves
-    o_netUpdateL[0] = t_real(0.0);
-    o_netUpdateL[1] = t_real(0.0);
-    o_netUpdateR[0] = t_real(0.0);
-    o_netUpdateR[1] = t_real(0.0);
-
     if (std::signbit(delta1)) {
-        o_netUpdateL[0] += z1[0];
-        o_netUpdateL[1] += z1[1];
+        o_netUpdateR[0] = t_real(0.0);
+        o_netUpdateR[1] = t_real(0.0);
+
+        o_netUpdateL[0] = z1[0];
+        o_netUpdateL[1] = z1[1];
     } else {
-        o_netUpdateR[0] += z1[0];
-        o_netUpdateR[1] += z1[1];
+        o_netUpdateL[0] = t_real(0.0);
+        o_netUpdateL[1] = t_real(0.0);
+
+        o_netUpdateR[0] = z1[0];
+        o_netUpdateR[1] = z1[1];
     }
 
     if (std::signbit(delta2)) {
